a2264e60b0fac984d06deeea6d0b3ac137a63509
DLeasure <DLeasure9@gmail.com>
2019-05-28 22:34:11 -0400

    last attempt at deleting

--- Javascript-address-book-lab-part-2.js
+++ Javascript-address-book-lab-part-2.js
@@ -57,8 +57,8 @@ class AddressBook {
             deleteButton.innerHTML = "delete"
             deleteButton.addEventListener('click', function(e) {
                 // console.log("pressed " + index);
-                e.preventDefault();
-                myAddressBook.deleteContactAt(index);
+                // e.preventDefault();
+                myAddressBook.contacts.splice(index, 1);
             })
             
             newDiv.appendChild(deleteButton);
@@ -80,6 +80,31 @@ document.getElementById('new-contact-info').addEventListener("submit", function(
     e.preventDefault();
     myAddressBook.printContactsAtEndOfHTML();
 })
+
+// for (let index = 0; index < this.contacts.length; index++) {
+//     // console.log(`${index}. ${this.contacts[index]["name"]} P: ${this.contacts[index]["phone"]} E: ${this.contacts[index]["email"]} R: ${this.contacts[index]["relation"]}`);
+//     let newDiv = document.createElement("div");
+//     newDiv.setAttribute('class', 'contact');
+//     // newDiv.setAttribute('position', 'relative');
+//     newDiv.innerHTML = (`Name: ${this.contacts[index]["name"]}<br />Email: ${this.contacts[index]["phone"]} <br />Email: ${this.contacts[index]["email"]} <br />Relation: ${this.contacts[index]["relation"]}`);
+//     // newDiv.innerHTML += <i class="material-icons">delete</i>;
+//     // newDiv.innerText = "Hello World!";
+//     // console.log(this.contacts[index]);
+
+//     let deleteButton = document.createElement("button");
+//     deleteButton.setAttribute('class', 'delete-button material-icons');
+//     // deleteButton.setAttribute('id', this.contacts[index]["name"]);
+//     deleteButton.innerHTML = "delete"
+//     deleteButton.addEventListener('click', function(e) {
+//         // console.log("pressed " + index);
+//         e.preventDefault();
+//         myAddressBook.deleteContactAt(index);
+//     })
+    
+//     newDiv.appendChild(deleteButton);
+//     let addressBookDiv = document.getElementById("address-book");
+//     addressBookDiv.append(newDiv);
+// }
 
 // let deleteLink = document.querySelectorAll('.delete-button');
 // for (var index = 0; index < deleteLink.length; index++) {
